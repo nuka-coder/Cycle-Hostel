07003b128bf65884125d56d02a2383c96fb2b2ae
houdinibeanie <jaybritany@gmail.com>
2022-03-05 23:19:02 -0500

    landing work

--- scripts.js
+++ scripts.js
@@ -21,3 +21,51 @@ $(".nav-link").on("mouseout", function() {
   $(this).removeClass("nav-link-effect");
   $(this).css("color", "white");
 })
+
+// HAMBURGER MENU //
+const navMenuIcon1 = $(".menu-icon-1"); //triple bar menu icon
+const navMenuIcon2 = $(".menu-icon-2"); //double bar menu icon
+const pageLinkMenu = $("#nav-links"); //menu links (i.e. Menu, Contact, Home)
+
+//function attached to 'click' event listener for mobile menu dropdown (click event applied in html)
+function mobileMenuClick1() {
+    navMenuIcon2.slideDown(); //appear double line menu icon
+    navMenuIcon1.css("display", "none");//hide triple line menu icon
+    pageLinkMenu.slideDown();//slide down menu links from navabr
+};
+function mobileMenuClick2() {
+  navMenuIcon2.css("display", "none");//hide double line menu icon
+  navMenuIcon1.slideDown();//appear triple line menu icon
+  pageLinkMenu.slideUp();//hide menu links dropdown
+};
+//function for mouseover event listener for menu icons
+function mobileMenuMouseover() {
+$(".menu-icon-1").css("color","#BBA484");
+$(".menu-icon-2").css("color","#BBA484");
+};
+
+function mobileMenuMouseleave() {
+$(".menu-icon-1").css("color","#fff");
+$(".menu-icon-2").css("color","#fff");
+};
+
+
+// Landing Background switch
+var heroImages = [
+  "images/hero5.jpg",
+  "images/hero2.jpg",
+  "images/hero3.jpg",
+  "images/hero4.jpg",
+  "images/hero1.jpg"
+]
+var imageHero = document.getElementById("Hero");
+
+var i = 0;
+setInterval(function() {
+      imageHero.style.backgroundImage = "url(" + heroImages[i] + ")";
+
+      i = i + 1;
+      if (i == heroImages.length) {
+        i =  0;
+      }
+}, 5000);
