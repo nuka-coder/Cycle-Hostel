c328cb6b42770c3190b936d45eae9db5b9f64304
houdinibeanie <jaybritany@gmail.com>
2022-03-10 00:07:34 -0500

    responsive landing and add mobile header

--- scripts.js
+++ scripts.js
@@ -2,52 +2,16 @@
 if ( window.history.replaceState ) {
   window.history.replaceState( null, null, window.location.href );
 }
-// Animation for .pageTitle
-$(document).on("load", function() {
-  $(h1).slideDown();
-})
-//Button Effects
-$(".book-btn").on("click", function() {
-  $(this).css("background-color", "#334147").slideUp(200).slideDown(300);
-  $(this).css("color", "white");
-})
 
-//Navbar Links Effects
-$(".nav-link").on("mouseover", function() {
-  $(this).addClass("nav-link-effect");
-  $(this).css("color", "#334147");
-})
-$(".nav-link").on("mouseout", function() {
-  $(this).removeClass("nav-link-effect");
-  $(this).css("color", "white");
-})
+// mobile menu
 
-// HAMBURGER MENU //
-const navMenuIcon1 = $(".menu-icon-1"); //triple bar menu icon
-const navMenuIcon2 = $(".menu-icon-2"); //double bar menu icon
-const pageLinkMenu = $("#nav-links"); //menu links (i.e. Menu, Contact, Home)
+function openMenu() {
 
-//function attached to 'click' event listener for mobile menu dropdown (click event applied in html)
-function mobileMenuClick1() {
-    navMenuIcon2.slideDown(); //appear double line menu icon
-    navMenuIcon1.css("display", "none");//hide triple line menu icon
-    pageLinkMenu.slideDown();//slide down menu links from navabr
-};
-function mobileMenuClick2() {
-  navMenuIcon2.css("display", "none");//hide double line menu icon
-  navMenuIcon1.slideDown();//appear triple line menu icon
-  pageLinkMenu.slideUp();//hide menu links dropdown
-};
-//function for mouseover event listener for menu icons
-function mobileMenuMouseover() {
-$(".menu-icon-1").css("color","#BBA484");
-$(".menu-icon-2").css("color","#BBA484");
-};
+  var navLinks = $(".mobile-nav-links");
+    $(".mobile-nav-links").slideToggle();
+    $(".mobile-nav-links").css("display", "flex");
+}
 
-function mobileMenuMouseleave() {
-$(".menu-icon-1").css("color","#fff");
-$(".menu-icon-2").css("color","#fff");
-};
 
 
 // Landing Background switch
